84b2c9f63ebe01e8cc49942bbcf5397b8a616719
nvlight <nvlight@mail.ru>
2019-10-02 17:25:14 +0300

    fc

---
+++ contacts.php
@@ -0,0 +1,41 @@
+<?php
+
+require 'pages/head.php';
+
+?>
+
+    <div class="page-wrap">
+        <?php
+
+        require 'pages/header.php';
+
+        ?>
+      <div class="inner-content center-bl markdown-body">
+	<h3>Контакты:</h3>
+	<p><strong>Email:</strong> info@cooldesk.pw</p>
+	<p><strong>Телефон:</strong> +7-(999)-333-77-55</p>
+    <p><strong>Реквизиты:</strong> ИП  А.В. Ройзман, ИНН 199707723478 г. Москва, ул. Пирогова, 21, офис 306</p>
+	<p>Обращаем ваше внимание что, наша мебель не представлена на сегодняшний день в шоу-румах, и может быть заказана только через наш интернет-магазин: <a href="index.html">Cooldesk.com</a></p>
+      </div>
+        <?php
+
+        require 'pages/footer.php';
+
+        ?>
+    </div>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,800" rel="stylesheet">
+    <link href="css/style.min.css" rel="stylesheet">
+    <link href="css/markdown.css" rel="stylesheet">
+    <!-- Owl Stylesheets (begin) -->
+    <link href="css/owl.carousel.min.css" rel="stylesheet">
+    <!-- Owl Stylesheets (end) -->
+    <!-- fancybox Stylesheets (begin) -->
+    <link href="css/jquery.fancybox.min.css" rel="stylesheet">
+    <!-- fancybox Stylesheets (end) -->
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/jquery.typed.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.fancybox.min.js"></script>
+
+  </body>
+</html>
